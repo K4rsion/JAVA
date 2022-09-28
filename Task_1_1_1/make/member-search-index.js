5c6dc85b8cd48b6d27bb3c2ae3d2f32ce1aad2b1
unknown <k.gurin@g.nsu.ru>
2022-09-28 13:59:07 +0700

    .gitignore fixed

--- Task_1_1_1/make/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"ru.nsu.kgurin","c":"Heapsort","l":"heapify(int[], int, int)","url":"heapify(int[],int,int)"},{"p":"ru.nsu.kgurin","c":"Heapsort","l":"Heapsort()","url":"%3Cinit%3E()"},{"p":"ru.nsu.kgurin","c":"Heapsort","l":"heapsort(int[])"},{"p":"ru.nsu.kgurin","c":"Heapsort","l":"main(String[])","url":"main(java.lang.String[])"}]
