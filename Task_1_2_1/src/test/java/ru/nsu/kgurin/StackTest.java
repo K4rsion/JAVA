18f1b62185657e1d56da7deea27c0f2cf074ede9
unknown <k.gurin@g.nsu.ru>
2022-10-03 14:13:39 +0700

    String test

--- Task_1_2_1/src/test/java/ru/nsu/kgurin/StackTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/kgurin/StackTest.java
@@ -2,7 +2,6 @@ package ru.nsu.kgurin;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import ru.nsu.kgurin.Stack;
 
 /**
  * Tests for a stack realization.
@@ -30,20 +29,20 @@ public class StackTest {
 
     @Test
     public void emptyStackTest(){
-        Stack<Integer> expected = new Stack<>(0);
+        Stack<Integer> expected = new Stack<>(1);
 
-        Stack<Integer> actual = new Stack<>(0);
+        Stack<Integer> actual = new Stack<>(1);
 
         Assertions.assertTrue(expected.stackAssertion(actual));
     }
 
     @Test
     public void boundExceededTest(){
-        Stack<Integer> expected = new Stack<>(0);
+        Stack<Integer> expected = new Stack<>(1);
         expected.pop();
         expected.push(2);
 
-        Stack<Integer> actual = new Stack<>(0);
+        Stack<Integer> actual = new Stack<>(1);
         actual.push(2);
         actual.push(7);
         actual.push(8);
@@ -54,7 +53,7 @@ public class StackTest {
 
     @Test
     public void equalPopAndPopStackTest(){
-        Stack<Integer> expected = new Stack<>(0);
+        Stack<Integer> expected = new Stack<>(1);
 
         Stack<Integer> actual = new Stack<>(2);
         actual.push(2);
@@ -67,17 +66,38 @@ public class StackTest {
 
     @Test
     public void reallocTest(){
-        Stack<Integer> expected = new Stack<>(0);
+        Stack<Integer> expected = new Stack<>(1);
         expected.push(1);
         expected.push(2);
         expected.push(3);
 
-        Stack<Integer> actual = new Stack<>(0);
+        Stack<Integer> actual = new Stack<>(1);
         actual.push(1);
-        Stack<Integer> s1 = new Stack<>(0);
+        Stack<Integer> s1 = new Stack<>(1);
         s1.push(2);
         s1.push(3);
         actual.pushStack(s1);
+
+        Assertions.assertTrue(expected.stackAssertion(actual));
+    }
+
+    @Test
+    public void stringTest(){
+        Stack<String> expected = new Stack<>(4);
+        expected.push("My");
+        expected.push("Name");
+        expected.push("Is");
+        expected.push("Kirill!");
+
+        Stack<String> actual = new Stack<>(2);
+        actual.push("My");
+        actual.push("Name");
+        Stack<String> s1 = new Stack<>(1);
+        s1.push("Is");
+        s1.push("Andrew!");
+        s1.pop();
+        actual.pushStack(s1);
+        actual.push("Kirill!");
 
         Assertions.assertTrue(expected.stackAssertion(actual));
     }
