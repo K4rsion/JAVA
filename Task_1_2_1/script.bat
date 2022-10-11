8ae0a012e23abcaa62aa0c2f5508d595c617a3e5
unknown <k.gurin@g.nsu.ru>
2022-10-11 15:24:46 +0700

    Repair of master branch structure

--- Task_1_2_1/script.bat
+++
@@ -1,8 +0,0 @@
-javadoc -d .\make\ -sourcepath .\src\main\java -subpackages ru.nsu.kgurin
-
-javac -d .\make\bin\ -sourcepath .\src\main\java .\src\main\java\ru\nsu\kgurin\Stack.java
-
-mkdir .\make\jar
-jar cf .\make\jar\Stack.jar -C .\make\bin .
-
-java -classpath .\make\jar\Stack.jar ru.nsu.kgurin.Stack
