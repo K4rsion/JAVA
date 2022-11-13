a05b21f633a119af8cb101fb7f4080610464b424
unknown <k.gurin@g.nsu.ru>
2022-11-13 21:59:08 +0700

    Task_1_2_3

---
+++ Task_1_2_3/src/test/java/ru/nsu/kgurin/EdgeTest.java
@@ -0,0 +1,73 @@
+package ru.nsu.kgurin;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EdgeTest {
+
+    // Atomic tests
+    @Test
+    void getVertexToTest() {
+        //actual
+        Vertex<Integer> actualVertexFrom = new Vertex<>(1);
+        Vertex<Integer> actualVertexTo = new Vertex<>(2);
+        Edge<Integer> actualEdge = new Edge<>(actualVertexFrom, actualVertexTo, 1);
+
+        //expected
+        Vertex<Integer> expectedVertexTo = new Vertex<>(2);
+
+        //asserts
+        Assertions.assertEquals(expectedVertexTo, actualEdge.getVertexTo());
+    }
+
+    @Test
+    void getVertexFromTest() {
+        //actual
+        Vertex<Integer> actualVertexFrom = new Vertex<>(1);
+        Vertex<Integer> actualVertexTo = new Vertex<>(2);
+        Edge<Integer> actualEdge = new Edge<>(actualVertexFrom, actualVertexTo, 1);
+
+        //expected
+        Vertex<Integer> expectedVertexTo = new Vertex<>(1);
+
+        //asserts
+        Assertions.assertEquals(expectedVertexTo, actualEdge.getVertexFrom());
+    }
+
+    @Test
+    void getWeightTest() {
+        //actual
+        Vertex<Integer> actualVertexFrom = new Vertex<>(1);
+        Vertex<Integer> actualVertexTo = new Vertex<>(2);
+        Edge<Integer> actualEdge = new Edge<>(actualVertexFrom, actualVertexTo, 1);
+
+        //asserts
+        Assertions.assertEquals(1, actualEdge.getWeight());
+    }
+
+    @Test
+    void equalsTest() {
+        Vertex<Integer> v1 = new Vertex<>(1);
+        Vertex<Integer> v2 = new Vertex<>(2);
+        Vertex<Integer> v3 = new Vertex<>(3);
+        Edge<Integer> e1 = new Edge<>(v1, v2, 1);
+        Edge<Integer> e2 = new Edge<>(v1, v2, 1);
+        Edge<Integer> e3 = new Edge<>(v1, v3, 1);
+
+        Assertions.assertEquals(e1, e2);
+        Assertions.assertNotEquals(e1, e3);
+    }
+
+    @Test
+    void hashCodeTest() {
+        Vertex<Integer> v1 = new Vertex<>(1);
+        Vertex<Integer> v2 = new Vertex<>(2);
+        Vertex<Integer> v3 = new Vertex<>(3);
+        Edge<Integer> e1 = new Edge<>(v1, v2, 1);
+        Edge<Integer> e2 = new Edge<>(v1, v2, 1);
+        Edge<Integer> e3 = new Edge<>(v1, v3, 1);
+
+        Assertions.assertEquals(e1.hashCode(), e2.hashCode());
+        Assertions.assertNotEquals(e1.hashCode(), e3.hashCode());
+    }
+}
---
+++ Task_1_2_3/src/test/java/ru/nsu/kgurin/VertexTest.java
@@ -0,0 +1,77 @@
+package ru.nsu.kgurin;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class VertexTest {
+
+    // Atomic tests
+    @Test
+    void getDistanceTest() {
+        //actual
+        Vertex<Integer> actualVertex = new Vertex<>(1);
+        actualVertex.setDistance(1);
+
+        //asserts
+        Assertions.assertEquals(1, actualVertex.getDistance());
+    }
+
+    @Test
+    void setDistanceTest() {
+        //actual
+        Vertex<Integer> actualVertex = new Vertex<>(1);
+        actualVertex.setDistance(1);
+
+        //asserts
+        Assertions.assertEquals(1, actualVertex.getDistance());
+    }
+
+    @Test
+    void getVisitedTest() {
+        //actual
+        Vertex<Integer> actualVertex = new Vertex<>(1);
+        actualVertex.setVisited(true);
+
+        //asserts
+        Assertions.assertTrue(actualVertex.getVisited());
+    }
+
+    @Test
+    void setVisitedTest() {
+        //actual
+        Vertex<Integer> actualVertex = new Vertex<>(1);
+        actualVertex.setVisited(true);
+
+        //asserts
+        Assertions.assertTrue(actualVertex.getVisited());
+    }
+
+    @Test
+    void getKeyTest() {
+        //actual
+        Vertex<Integer> actualVertex = new Vertex<>(1);
+
+        //asserts
+        Assertions.assertEquals(1, actualVertex.getKey());
+    }
+
+    @Test
+    void equalsTest() {
+        Vertex<Integer> v1 = new Vertex<>(1);
+        Vertex<Integer> v2 = new Vertex<>(1);
+        Vertex<Integer> v3 = new Vertex<>(2);
+
+        Assertions.assertEquals(v1, v2);
+        Assertions.assertNotEquals(v1, v3);
+    }
+
+    @Test
+    void hashCodeTest() {
+        Vertex<Integer> v1 = new Vertex<>(1);
+        Vertex<Integer> v2 = new Vertex<>(1);
+        Vertex<Integer> v3 = new Vertex<>(2);
+
+        Assertions.assertEquals(v1.hashCode(), v2.hashCode());
+        Assertions.assertNotEquals(v1.hashCode(), v3.hashCode());
+    }
+}
