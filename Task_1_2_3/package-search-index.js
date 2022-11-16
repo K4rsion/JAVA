a6ea7941b666429f721febd2e9446bd17bbe334f
K4rsion <K4rsion@users.noreply.github.com>
2022-11-16 07:03:43 +0000

    Deploying to gh-pages from @ K4rsion/JAVA@555fee7cb27dcd3645c587128856c81033b8c44e 🚀

--- Task_1_2_3/package-search-index.js
+++ Task_1_2_3/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.kgurin"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.kgurin"}];updateSearchResults();
--- Task_1_2_3/type-search-index.js
+++ Task_1_2_3/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.kgurin","l":"Edge"},{"p":"ru.nsu.kgurin","l":"Graph"},{"p":"ru.nsu.kgurin","l":"Vertex"}]
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.kgurin","l":"Edge"},{"p":"ru.nsu.kgurin","l":"Graph"},{"p":"ru.nsu.kgurin","l":"Vertex"}];updateSearchResults();
