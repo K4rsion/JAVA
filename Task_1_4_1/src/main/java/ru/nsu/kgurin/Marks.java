f3c27ff21cfb24224e573bdaaa727bd037379eca
unknown <k.gurin@g.nsu.ru>
2022-12-10 22:49:46 +0700

    ReviewDog fixed

--- Task_1_4_1/src/main/java/ru/nsu/kgurin/Marks.java
+++ Task_1_4_1/src/main/java/ru/nsu/kgurin/Marks.java
@@ -4,14 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Handle class for wrapping a map: Subject -> Mark.
+ * Handle class for wrapping a map: Subject, Mark.
  */
 public class Marks {
 
     private final Map<String, Integer> subjectMark = new HashMap<>();
 
     /**
-     * Handle function to get a map: Subject -> Mark.
+     * Handle function to get a map: Subject, Mark.
+     *
+     * @return map: Subject, Mark
      */
     public Map<String, Integer> getSubjectMark() {
         return subjectMark;
--- Task_1_4_1/src/main/java/ru/nsu/kgurin/RecordBook.java
+++ Task_1_4_1/src/main/java/ru/nsu/kgurin/RecordBook.java
@@ -14,6 +14,8 @@ public class RecordBook {
 
     /**
      * Constructor for RecordBook class.
+     *
+     * @param diplomaMark mark for diploma work
      */
     public RecordBook(int diplomaMark) {
         this.diplomaWork = diplomaMark;
