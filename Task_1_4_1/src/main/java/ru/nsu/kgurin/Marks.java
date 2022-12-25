6d6503a4475040f554f806e22f249cbeb34ebd3d
unknown <k.gurin@g.nsu.ru>
2022-12-25 16:58:29 +0700

    Correct structure

---
+++ Task_1_4_1/src/main/java/ru/nsu/kgurin/Marks.java
@@ -0,0 +1,21 @@
+package ru.nsu.kgurin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Handle class for wrapping a map: Subject, Mark.
+ */
+public class Marks {
+
+    private final Map<String, Integer> subjectMark = new HashMap<>();
+
+    /**
+     * Handle function to get a map: Subject, Mark.
+     *
+     * @return map: Subject, Mark
+     */
+    public Map<String, Integer> getSubjectMark() {
+        return subjectMark;
+    }
+}
