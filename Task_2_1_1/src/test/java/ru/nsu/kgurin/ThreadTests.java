dd04e7b5a11d9c1638392ca9078779836e00dd26
unknown <k.gurin@g.nsu.ru>
2023-02-28 23:53:35 +0700

    ReviewDog fixed

--- Task_2_1_1/src/test/java/ru/nsu/kgurin/ThreadTests.java
+++ Task_2_1_1/src/test/java/ru/nsu/kgurin/ThreadTests.java
@@ -1,17 +1,16 @@
 package ru.nsu.kgurin;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /**
  * Tests that checks the correctness of ThreadSolve class.
  */
 public class ThreadTests {
     @Test
-    public void PrimeTest1() throws InterruptedException {
+    public void PrimeTestOne() throws InterruptedException {
         List<Integer> list = Arrays.asList(1, 2, 3);
         ThreadSolve test = new ThreadSolve();
         boolean act = test.threadSolve(list, 1);
@@ -20,7 +19,7 @@ public class ThreadTests {
     }
 
     @Test
-    public void PrimeTest2() throws InterruptedException {
+    public void PrimeTestTwo() throws InterruptedException {
         List<Integer> list = Arrays.asList(999917, 999931, 999953, 999959, 999961, 999979, 999983);
         ThreadSolve test = new ThreadSolve();
         boolean act = test.threadSolve(list, 2);
@@ -29,7 +28,7 @@ public class ThreadTests {
     }
 
     @Test
-    public void PrimeTest3() throws InterruptedException {
+    public void PrimeTestThree() throws InterruptedException {
         List<Integer> list = Arrays.asList(945031, 945037, 945059, 945089, 945103, 945143, 945151, 945179, 945209, 945211);
         ThreadSolve test = new ThreadSolve();
         boolean act = test.threadSolve(list, 3);
@@ -38,7 +37,7 @@ public class ThreadTests {
     }
 
     @Test
-    public void CompositeTest1() throws InterruptedException {
+    public void CompositeTestOne() throws InterruptedException {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
         ThreadSolve test = new ThreadSolve();
         boolean act = test.threadSolve(list, 2);
@@ -47,7 +46,7 @@ public class ThreadTests {
     }
 
     @Test
-    public void CompositeTest2() throws InterruptedException {
+    public void CompositeTestTwo() throws InterruptedException {
         List<Integer> list = Arrays.asList(94530, 945030, 945050, 945210, 945210);
         ThreadSolve test = new ThreadSolve();
         boolean act = test.threadSolve(list, 3);
@@ -56,7 +55,7 @@ public class ThreadTests {
     }
 
     @Test
-    public void CompositeTest3() throws InterruptedException {
+    public void CompositeTestThree() throws InterruptedException {
         List<Integer> list = Arrays.asList(945032, 945037, 945059, 945089, 945103, 945143, 945151, 945179, 945209, 945211);
         ThreadSolve test = new ThreadSolve();
         boolean act = test.threadSolve(list, 3);
