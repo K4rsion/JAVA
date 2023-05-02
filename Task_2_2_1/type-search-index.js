07597ebf09857db2c3db0c8f21ec1ee5c87669b4
K4rsion <K4rsion@users.noreply.github.com>
2023-05-02 08:33:10 +0000

    Deploying to gh-pages from @ K4rsion/JAVA@2be905915e9803a65f38dbfaff44dd3896563153 🚀

--- Task_2_2_1/type-search-index.js
+++ Task_2_2_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.kgurin","l":"Baker"},{"p":"ru.nsu.kgurin","l":"CreateOrder"},{"p":"ru.nsu.kgurin","l":"Deliverer"},{"p":"ru.nsu.kgurin","l":"Order"},{"p":"ru.nsu.kgurin","l":"Pizzeria"},{"p":"ru.nsu.kgurin","l":"Staff"},{"p":"ru.nsu.kgurin","l":"Stock"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.kgurin","l":"Baker"},{"p":"ru.nsu.kgurin","l":"Deliverer"},{"p":"ru.nsu.kgurin","l":"JsonData"},{"p":"ru.nsu.kgurin","l":"Main"},{"p":"ru.nsu.kgurin","l":"Order"}]
