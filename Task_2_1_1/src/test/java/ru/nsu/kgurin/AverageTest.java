7334905c620d9bb94e2f99796fc944855ab91499
kenion <k.gurin@g.nsu.ru>
2023-05-02 13:49:54 +0700

    resolve conflicts

---
+++ Task_2_1_1/src/test/java/ru/nsu/kgurin/AverageTest.java
@@ -0,0 +1,87 @@
+package ru.nsu.kgurin;
+
+import java.util.Arrays;
+
+/**
+ * Test for measuring average time of executing big tests.
+ */
+public class AverageTest {
+    /**
+     * Main method.
+     */
+    public static void main(String[] args) throws InterruptedException {
+        TimeComplexityTests test = new TimeComplexityTests();
+        Long[][] data = new Long[13][5];
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Test repeat № " + (i + 1) + " - started");
+            data[0][i] = test.sequentialPrimeTest();
+            data[1][i] = test.parallelStreamPrimeTest();
+            data[2][i] = test.threadsPrimeTest1();
+            data[3][i] = test.threadsPrimeTest2();
+            data[4][i] = test.threadsPrimeTest4();
+            data[5][i] = test.threadsPrimeTest8();
+            data[6][i] = test.threadsPrimeTest16();
+            data[7][i] = test.threadsPrimeTest32();
+            data[8][i] = test.threadsPrimeTest64();
+            data[9][i] = test.threadsPrimeTest128();
+            data[10][i] = test.threadsPrimeTest256();
+            data[11][i] = test.threadsPrimeTest256();
+            data[12][i] = test.threadsPrimeTest256();
+            System.out.println("Test repeat № " + (i + 1) + " - finished");
+        }
+        System.out.println("============Tests "
+                + "finished===========");
+        System.out.println("Sequential average: "
+                + Arrays.stream(data[0])
+                .mapToLong(Long::longValue)
+                .average());
+        System.out.println("ParallelStream average: "
+                + Arrays.stream(data[1])
+                .mapToLong(Long::longValue)
+                .average());
+        System.out.println("1 thread average: "
+                + Arrays.stream(data[2])
+                .mapToLong(Long::longValue)
+                .average());
+        System.out.println("2 thread average: "
+                + Arrays.stream(data[3])
+                .mapToLong(Long::longValue)
+                .average());
+        System.out.println("4 thread average: "
+                + Arrays.stream(data[4])
+                .mapToLong(Long::longValue)
+                .average());
+        System.out.println("8 thread average: "
+                + Arrays.stream(data[5])
+                .mapToLong(Long::longValue)
+                .average());
+        System.out.println("16 thread average: "
+                + Arrays.stream(data[6])
+                .mapToLong(Long::longValue)
+                .average());
+        System.out.println("32 thread average: "
+                + Arrays.stream(data[7])
+                .mapToLong(Long::longValue)
+                .average());
+        System.out.println("64 thread average: "
+                + Arrays.stream(data[8])
+                .mapToLong(Long::longValue)
+                .average());
+        System.out.println("128 thread average: "
+                + Arrays.stream(data[9])
+                .mapToLong(Long::longValue)
+                .average());
+        System.out.println("256 thread average: "
+                + Arrays.stream(data[10])
+                .mapToLong(Long::longValue)
+                .average());
+        System.out.println("512 thread average: "
+                + Arrays.stream(data[11])
+                .mapToLong(Long::longValue)
+                .average());
+        System.out.println("1024 thread average: "
+                + Arrays.stream(data[12])
+                .mapToLong(Long::longValue)
+                .average());
+    }
+}
---
+++ Task_2_1_1/src/test/java/ru/nsu/kgurin/TimeComplexityTests.java
@@ -0,0 +1,205 @@
+package ru.nsu.kgurin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Complex tests for time measuring.
+ */
+public class TimeComplexityTests {
+    private final List<Integer> primeList = initPrimeList();
+
+    /**
+     * Initialization of list contains only prime numbers.
+     *
+     * @return list with prime numbers
+     */
+    private List<Integer> initPrimeList() {
+        List<Integer> result = new ArrayList<>();
+        long size = 10_000_000;
+        for (long i = 0; i < size; i++) {
+            result.add(100003);
+        }
+        return result;
+    }
+
+    /**
+     * Test.
+     *
+     * @return timeFinish
+     */
+    public long sequentialPrimeTest() {
+        long timeStart = System.currentTimeMillis();
+        SequentialSolve.sequentialSolve(primeList);
+        long timeFinish = System.currentTimeMillis() - timeStart;
+        System.out.println("Sequential test: " + timeFinish);
+        return timeFinish;
+    }
+
+    /**
+     * Test.
+     *
+     * @return timeFinish
+     */
+    public long parallelStreamPrimeTest() {
+        long timeStart = System.currentTimeMillis();
+        ParallelStreamSolve.parallelStreamSolve(primeList);
+        long timeFinish = System.currentTimeMillis() - timeStart;
+        System.out.println("ParallelStream test: " + timeFinish);
+        return timeFinish;
+    }
+
+    /**
+     * Test.
+     *
+     * @return timeFinish
+     */
+    public long threadsPrimeTest1() throws InterruptedException {
+        long timeStart = System.currentTimeMillis();
+        ThreadSolve test = new ThreadSolve();
+        test.threadSolve(primeList, 1);
+        long timeFinish = System.currentTimeMillis() - timeStart;
+        System.out.println("1 thread test: " + timeFinish);
+        return timeFinish;
+    }
+
+    /**
+     * Test.
+     *
+     * @return timeFinish
+     */
+    public long threadsPrimeTest2() throws InterruptedException {
+        long timeStart = System.currentTimeMillis();
+        ThreadSolve test = new ThreadSolve();
+        test.threadSolve(primeList, 2);
+        long timeFinish = System.currentTimeMillis() - timeStart;
+        System.out.println("2 thread test: " + timeFinish);
+        return timeFinish;
+    }
+
+    /**
+     * Test.
+     *
+     * @return timeFinish
+     */
+    public long threadsPrimeTest4() throws InterruptedException {
+        long timeStart = System.currentTimeMillis();
+        ThreadSolve test = new ThreadSolve();
+        test.threadSolve(primeList, 4);
+        long timeFinish = System.currentTimeMillis() - timeStart;
+        System.out.println("4 thread test: " + timeFinish);
+        return timeFinish;
+    }
+
+    /**
+     * Test.
+     *
+     * @return timeFinish
+     */
+    public long threadsPrimeTest8() throws InterruptedException {
+        long timeStart = System.currentTimeMillis();
+        ThreadSolve test = new ThreadSolve();
+        test.threadSolve(primeList, 8);
+        long timeFinish = System.currentTimeMillis() - timeStart;
+        System.out.println("8 thread test: " + timeFinish);
+        return timeFinish;
+    }
+
+    /**
+     * Test.
+     *
+     * @return timeFinish
+     */
+    public long threadsPrimeTest16() throws InterruptedException {
+        long timeStart = System.currentTimeMillis();
+        ThreadSolve test = new ThreadSolve();
+        test.threadSolve(primeList, 16);
+        long timeFinish = System.currentTimeMillis() - timeStart;
+        System.out.println("16 thread test: " + timeFinish);
+        return timeFinish;
+    }
+
+    /**
+     * Test.
+     *
+     * @return timeFinish
+     */
+    public long threadsPrimeTest32() throws InterruptedException {
+        long timeStart = System.currentTimeMillis();
+        ThreadSolve test = new ThreadSolve();
+        test.threadSolve(primeList, 32);
+        long timeFinish = System.currentTimeMillis() - timeStart;
+        System.out.println("32 thread test: " + timeFinish);
+        return timeFinish;
+    }
+
+    /**
+     * Test.
+     *
+     * @return timeFinish
+     */
+    public long threadsPrimeTest64() throws InterruptedException {
+        long timeStart = System.currentTimeMillis();
+        ThreadSolve test = new ThreadSolve();
+        test.threadSolve(primeList, 64);
+        long timeFinish = System.currentTimeMillis() - timeStart;
+        System.out.println("64 thread test: " + timeFinish);
+        return timeFinish;
+    }
+
+    /**
+     * Test.
+     *
+     * @return timeFinish
+     */
+    public long threadsPrimeTest128() throws InterruptedException {
+        long timeStart = System.currentTimeMillis();
+        ThreadSolve test = new ThreadSolve();
+        test.threadSolve(primeList, 128);
+        long timeFinish = System.currentTimeMillis() - timeStart;
+        System.out.println("128 thread test: " + timeFinish);
+        return timeFinish;
+    }
+
+    /**
+     * Test.
+     *
+     * @return timeFinish
+     */
+    public long threadsPrimeTest256() throws InterruptedException {
+        long timeStart = System.currentTimeMillis();
+        ThreadSolve test = new ThreadSolve();
+        test.threadSolve(primeList, 256);
+        long timeFinish = System.currentTimeMillis() - timeStart;
+        System.out.println("256 thread test: " + timeFinish);
+        return timeFinish;
+    }
+
+    /**
+     * Test.
+     *
+     * @return timeFinish
+     */
+    public long threadsPrimeTest512() throws InterruptedException {
+        long timeStart = System.currentTimeMillis();
+        ThreadSolve test = new ThreadSolve();
+        test.threadSolve(primeList, 512);
+        long timeFinish = System.currentTimeMillis() - timeStart;
+        System.out.println("512 thread test: " + timeFinish);
+        return timeFinish;
+    }
+
+    /**
+     * Test.
+     *
+     * @return timeFinish
+     */
+    public long threadsPrimeTest1024() throws InterruptedException {
+        long timeStart = System.currentTimeMillis();
+        ThreadSolve test = new ThreadSolve();
+        test.threadSolve(primeList, 1024);
+        long timeFinish = System.currentTimeMillis() - timeStart;
+        System.out.println("1024 thread test: " + timeFinish);
+        return timeFinish;
+    }
+}
